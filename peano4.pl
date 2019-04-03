53695f19efd1babd4ab3d42e634bfbc7f6080320
teunts <48525158+teunts@users.noreply.github.com>
2019-04-03 18:02:38 +0200

    Add files via upload

--- peano4.pl
+++ peano4.pl
@@ -1,5 +1,10 @@
 
 
+% sum(?X, ?Y, ?Z)
+
+sum(X, 0, X).
+sum(X, n(Y), n(Z)):- sum(X,Y,Z).
+
 d2p(0, 0).
 d2p(D, n(P)):- D > 0, D2 is D-1, d2p(D2, P).
 
@@ -12,3 +17,13 @@ p2d(n(X),  D2) :- p2d(X, D), D2 is D + 1.
 
 substr(X, 0, X).
 substr(X, n(Y), Z):- substr(X, Y, n(Z)).
+
+
+% times(?X, ?Y, ?Z)
+% it is true if Z unify with X * Y in Peano Artithmetic
+% it is true if Z unify with a Peano Number
+% equivalent to add X, Y times
+
+times(_, 0, 0).
+
+times(X, n(Y), R):- times(X, Y, Z), sum(X, Z, R).
